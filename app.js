d5c731c44b67b88dfd1ef6db559e4b4568d6803c
Nazar868 <zagariynazar04@gmail.com>
2026-06-16 17:53:42 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import { errors } from 'celebrate'
+import announcementsRouter from './src/routes/announcements.routes.js'
+
+const app = express()
+
+app.use(express.json())
+
+app.use('/announcements', announcementsRouter)
+
+// celebrate errors
+app.use(errors())
+
+// Prisma P2025 → 404
+app.use((err, req, res, next) => {
+  if (err.code === 'P2025') {
+    return res.status(404).json({ message: 'Not found' })
+  }
+
+  return res.status(500).json({ message: 'Server error' })
+})
+
+export default app
